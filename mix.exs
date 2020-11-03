ef19ea29059777a0bec65c36ddc0bc9fba9ff74e
Natassia Tavares <natassia.tavares@deliverycenter.com>
2020-11-03 11:01:29 -0300

    updated version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DCMetrics.MixProject do
   def project do
     [
       app: :dc_metrics,
-      version: "0.1.0",
+      version: "0.1.1",
       description: description(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
