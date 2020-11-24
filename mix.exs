4aead9150cc1be7ab36e841d6f8009c8f2d69eb0
Vitor Bortolotti <bortolotti.vitor@gmail.com>
2020-11-24 11:32:20 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DCMetrics.MixProject do
   def project do
     [
       app: :dc_metrics,
-      version: "0.1.1",
+      version: "0.1.2",
       description: description(),
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
